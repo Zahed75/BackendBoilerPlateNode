2bd42940546fc1efb54469adb8aa0609d14096bf
“Zahed75” <zahedhasan.cs@gmail.com>
2023-11-15 15:05:25 +0600

    new file:   .env 	new file:   .gitignore 	new file:   app.js 	new file:   index.js 	new file:   package-lock.json 	new file:   package.json 	new file:   src/config/mongo.js 	new file:   src/middlewares/authMiddleware.js 	new file:   src/middlewares/roleMiddleware.js 	new file:   src/middlewares/schemaValidation.js 	new file:   src/routes/index.js

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+//middlewares
+const authVerifyMiddleware = require('../middlewares/authMiddleware.js');
+
+
+// Root End Point
+
+
+module.exports = router;
