e852b339abbcc1843f65d9c63d7b3fc40fd05ae4
“Zahed75” <zahedhasan.cs@gmail.com>
2023-11-16 21:04:05 +0600

    new file:   src/config/constant.js 	new file:   src/modules/Auth/controller.js 	new file:   src/modules/Auth/request.js 	new file:   src/modules/Auth/service.js 	new file:   src/modules/User/controller.js 	new file:   src/modules/User/model.js 	new file:   src/modules/User/request.js 	new file:   src/modules/User/service.js 	modified:   src/routes/index.js 	new file:   src/utility/common.js 	new file:   src/utility/email.js 	new file:   src/utility/multer.js 	new file:   src/utility/sslOTP.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 //middlewares
-const authVerifyMiddleware = require('../middlewares/authMiddleware.js');
+// const authVerifyMiddleware = require('../middlewares/authMiddleware.js');
+
+
+//routes
+// const authRoute = require('../modules/Auth/controller');
+
 
 
 // Root End Point
-
+// router.use('/auth', authRoute);
 
 module.exports = router;
